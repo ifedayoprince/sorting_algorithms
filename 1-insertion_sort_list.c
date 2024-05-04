32cacf599ef67d19d59105a93bca3180e2532ef0
ifedayoprince <ifedayoprince@gmail.com>
2024-05-04 09:10:14 +0100

    fix: resolve constant issues

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -7,29 +7,39 @@
 */
 void insertion_sort_list(listint_t **list)
 {
-	listint_t *current, *temp;
+	listint_t *current, *temp, *prev;
 	int value;
 
-	if (!list)
+	if (!list || !*list)
 		return;
 
 	current = *list;
 	while (current)
 	{
 		temp = current->next;
+		prev = current;
 		while (temp)
 		{
 			if (current->n > temp->n)
 			{
-				value = current->n;
-				current->n = temp->n;
-				temp->n = value;
-				current = *list;
+				value = temp->n;
+				*(int *)&temp->n = current->n;
+				*(int *)&current->n = value;
+				if (current == *list)
+					*list = temp;
+				else
+					prev->next = temp;
+				prev = current;
+				current = temp->next;
+				temp = current;
 
 				print_list(*list);
-				break;
 			}
-			temp = temp->next;
+			else
+			{
+				prev = temp;
+				temp = temp->next;
+			}
 		}
 		current = current->next;
 	}
